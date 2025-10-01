0103022e551cc8b71580d0187ad55a74a09f7e33
videlanicolas <videlanicolas@gmail.com>
2025-10-01 01:01:31 +0000

    Changes

--- _site/assets/js/photoswipe-setup.js
+++
@@ -1,8 +0,0 @@
-import PhotoSwipeLightbox from "";
-import PhotoSwipe from "";
-const photoswipe = new PhotoSwipeLightbox({
-  gallery: ".pswp-gallery",
-  children: "a",
-  pswpModule: PhotoSwipe,
-});
-photoswipe.init();
